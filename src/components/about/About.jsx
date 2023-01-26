e6f95724d5c680d8788909d103d0515105107cb8
SANDEEEP259 <80709077+SANDEEEP259@users.noreply.github.com>
2023-01-26 13:45:01 +0530

    first commit

---
+++ src/components/about/About.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import "./about.css";
+import ME from "../../assets/sandeep.jpg";
+import { FaAward } from "react-icons/fa";
+import { FiUsers } from "react-icons/fi";
+import { VscFolderLibrary } from "react-icons/vsc";
+
+const About = () => {
+  return (
+    <section id="about">
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+
+      <div className="container about_container">
+        <div className="about_me">
+          <div className="about_me-image">
+            <img src={ME} alt="About Image" />
+          </div>
+        </div>
+
+        <div className="about_content">
+          <div className="about_cards">
+            <article className="about_card">
+              <FaAward className="about_icon"/>
+              <h5>Experience</h5>
+              <small>3+ years working</small>
+            </article>
+
+            <article className="about_card">
+              <FiUsers className="about_icon"/>
+              <h5>Clients</h5>
+              <small>10+ in India</small>
+            </article>
+
+            <article className="about_card">
+              <VscFolderLibrary  className="about_icon"/>
+              <h5>Projects</h5>
+              <small>3+ Completed</small>
+            </article>
+          </div>
+
+          <p className="">
+          It is a long established fact that a reader  will be distracted by the readable content of a 
+          page when looking at its layout. The point of using lorem Ipsum is that it has a more-normal 
+          distribution of letters, as opposed.
+          </p>
+
+          <a href="#contact" className="btn btn-primary">Let's Talk</a>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
